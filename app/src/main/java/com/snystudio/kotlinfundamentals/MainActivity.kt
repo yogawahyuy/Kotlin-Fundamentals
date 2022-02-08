a21ae663a908a211a7126935eaa34ed170e8b735
Yoga <yogawahyuy@gmail.com>
2022-02-08 13:58:03 +0700

    RollDice

---
+++ app/src/main/java/com/snystudio/kotlinfundamentals/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.snystudio.kotlinfundamentals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.btnId)
+        rollButton.setOnClickListener { rollDice()}
+    }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.text_cube)
+
+        resultText.text=randomInt.toString()
+    }
+}
