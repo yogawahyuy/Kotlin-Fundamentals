961dc213fc921c08db3c81be9762bddce2c21d10
Yoga <yogawahyuy@gmail.com>
2022-02-08 14:20:23 +0700

    RollDice with image

--- app/src/main/java/com/snystudio/kotlinfundamentals/MainActivity.kt
+++ app/src/main/java/com/snystudio/kotlinfundamentals/MainActivity.kt
@@ -3,22 +3,30 @@ package com.snystudio.kotlinfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage=findViewById(R.id.text_cube)
         val rollButton: Button = findViewById(R.id.btnId)
         rollButton.setOnClickListener { rollDice()}
     }
 
     private fun rollDice(){
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.text_cube)
-
-        resultText.text=randomInt.toString()
+        val randInt=(1..6).random()
+        val drawableResource = when(randInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
